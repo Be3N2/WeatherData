1d63dc77a7c3ece1a2aabd35b00b28d4784bf08a
Be3N2 <2017WakefieldB@gmail.com>
2019-02-16 16:04:39 -0500

    Fixed relative file locations, also added p5.js code for abs grid

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var saveLocation = "data.txt";
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+	res.sendFile(__dirname + '/public/index.html');
 });
 
 
@@ -21,24 +21,25 @@ var port = process.env.PORT || 3000;
 
 // open port with a callback
 var listener = app.listen(port, function () {
-  console.log('Your app is listening on port ' + port);
+	console.log('Your app is listening on port ' + port);
 });
 
 app.get('/save', function(req, res){
-  var stream = APIrequest(simpleRequest).pipe(fs.createWriteStream(saveLocation));
-  stream.on('finish', function () { 
-	res.sendFile(__dirname + '/public/saveScreen.html')
-  });
-  
+	//explanation on stream object https://stackoverflow.com/questions/11447872/callback-to-handle-completion-of-pipe
+	var stream = APIrequest(simpleRequest).pipe(fs.createWriteStream(saveLocation));
+	stream.on('finish', function () { 
+		res.sendFile(__dirname + '/public/saveScreen.html')
+	});
+
 });
 
 app.get('/visualize', function(req, res){
-  APIrequest(simpleRequest, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      var info = JSON.parse(body);
-      // do more stuff
-      console.log(info);
-      res.send(info);
-    }
-  })
+	APIrequest(simpleRequest, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var info = JSON.parse(body);
+			// do more stuff
+			console.log(info);
+			res.send(info);
+		}
+	})
 });
--- public/js/startup.js
+++ public/js/startup.js
@@ -1,13 +1,25 @@
 
+var blanketHeight = 20;
+var blanketWidth = 19;
+var blockWidth = 20;
 
 function setup() {
-	
+	createCanvas(blanketWidth * blockWidth + 1, blanketHeight * blockWidth + 1);
+	background(51);
+	for (var y = 0; y < blanketHeight; y++) {
+		for (var x = 0; x < blanketWidth; x++) {
+			console.log(x + "," + y);
+			var locationx = x * blockWidth;
+			var locationy = y * blockWidth;
+			rect(locationx, locationy, blockWidth, blockWidth);
+		}
+	}
 }
+
 
 function draw() {
 	
 }
-
 function mouseClicked() {
 
 }
