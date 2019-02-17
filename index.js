72e7bbe683cd104e3aa75e5d33d5a8fcf7e7c600
Be3N2 <2017WakefieldB@gmail.com>
2019-02-16 21:27:50 -0500

    Added grid for future weather data display, also started ravenna historical data

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ var express = require('express');
 var app = express();
 var APIrequest = require('request');
 var fs = require('fs');
-
 var simpleRequest = "https://api.darksky.net/forecast/f87eebf5030c5df083e9201182cb560c/41.1941795,-80.7491454?exclude=minutely,hourly,alerts,flags";
 var saveLocation = "data.txt";
 
@@ -16,7 +15,7 @@ app.get("/", function (req, res) {
 });
 
 
-//app.get("/shorten/*", addUrl);                                    
+//app.get("/shorten/*", addUrl);
 var port = process.env.PORT || 3000;
 
 // open port with a callback
@@ -28,7 +27,7 @@ app.get('/save', function(req, res){
 	//explanation on stream object https://stackoverflow.com/questions/11447872/callback-to-handle-completion-of-pipe
 	var stream = APIrequest(simpleRequest).pipe(fs.createWriteStream(saveLocation));
 	stream.on('finish', function () { 
-		res.sendFile(__dirname + '/public/saveScreen.html')
+		res.sendFile(__dirname + '/public/saveScreen.html');
 	});
 
 });
@@ -42,4 +41,45 @@ app.get('/visualize', function(req, res){
 			res.send(info);
 		}
 	})
+});
+
+app.get('/ravenna', function(req, res){
+	//loop ravenna calls
+	var ravennaWeather = {"weatherData":[]};
+	const monthNames = ["January", "February", "March", "April", "May", "June",
+	  "July", "August", "September", "October", "November", "December"
+	];
+	var ravennaFirstPart = "https://api.darksky.net/forecast/f87eebf5030c5df083e9201182cb560c/41.1575566,-81.2420473,";
+	var ravennaSecondPart = "?exclude=minutely,hourly,alerts,flags";
+
+	var date = new Date("28-May-2018 17:00:00");
+	var unixtime = date.getTime()/1000;
+
+	//for loop start
+	//decide on exact time
+	var ravennaRequest = ravennaFirstPart + unixtime + ravennaSecondPart;
+	var daymonthyear = date.getDate() + "-" + monthNames[date.getMonth()] + "-" + date.getFullYear();
+	var input = {"day": daymonthyear,"unixTime": unixtime, "data": {}};
+
+	APIrequest(ravennaRequest, function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var info = JSON.parse(body);
+			// do more stuff
+			input.data = info;
+		}
+	});
+	ravennaWeather.weatherData[0] = input;
+	//for loop end
+
+	//write data
+	var ravennaSave = "ravenna.txt";
+	fs.writeFile(ravennaSave, JSON.stringify(ravennaWeather), function(err) {
+	    if(err) {
+	        return console.log(err);
+	    }
+
+	    console.log("The file was saved!");
+	}); 
+
+	res.send(ravennaWeather);
 });
--- public/js/startup.js
+++ public/js/startup.js
@@ -3,17 +3,65 @@ var blanketHeight = 20;
 var blanketWidth = 19;
 var blockWidth = 20;
 
+var blue1 = "#667a86";
+var teal1 = "#92b0a9";
+var darkgreen1 = "#353708";
+var green1 = "#9b9844";
+var yellow1 = "#fdd57d";
+var pink1 = "#d6a7b4";
+
+//possible weather values
+//clear-day, clear-night, rain, snow, sleet, wind, fog, cloudy, partly-cloudy-day, or partly-cloudy-night
+
 function setup() {
 	createCanvas(blanketWidth * blockWidth + 1, blanketHeight * blockWidth + 1);
 	background(51);
 	for (var y = 0; y < blanketHeight; y++) {
 		for (var x = 0; x < blanketWidth; x++) {
-			console.log(x + "," + y);
+			var colorCode;
+			switch (Math.trunc(random(6))) {
+				case 0:
+					colorCode = yellow1;
+					break;
+				case 1:
+					colorCode = darkgreen1;
+					break;
+				case 2:
+					colorCode = green1;
+					break;
+				case 3:
+					colorCode = teal1;
+					break;
+				case 4:
+					colorCode = blue1;
+					break;
+				case 5:
+				default:
+					colorCode = pink1;
+			}
+			fill(colorCode);
 			var locationx = x * blockWidth;
 			var locationy = y * blockWidth;
 			rect(locationx, locationy, blockWidth, blockWidth);
+
+
 		}
-	}
+	}	
+	//UTC time is 5 hours ahead
+	var date = new Date("28-May-2018 17:00:00");
+	var unixtime = date.getTime()/1000;
+	//one day 86400
+	console.log(unixtime);
+	console.log(unixtime + 86400);
+
+	//daylight savings
+	var date = new Date("3-Nov-2018 17:00:00");
+	var unixtime = date.getTime()/1000;
+	console.log(unixtime);
+	console.log(unixtime + 86400);
+
+	//28-May 14-Oct in Ravenna
+	//15 Oct Today in Greensville
 }
 
 
